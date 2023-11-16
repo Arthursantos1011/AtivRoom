3115ea19f214e45c4392f72d6d82969fba301546
Arthursantos1011 <arthur.santos210@etec.sp.gov.br>
2023-11-16 09:06:33 -0300

    Ativroom

---
+++ app/src/main/java/com/example/approomaula/controller/ContatoDao.kt
@@ -0,0 +1,24 @@
+package com.example.approomaula.controller
+
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+
+interface ContatoDao {
+    @Upsert
+    suspend fun upsertContato(contato: Contato)
+
+    @Delete
+    suspend fun deleteContato(contato: Contato)
+
+    @Query("SELECT * FROM contatos ORDER BY nome ASC")
+    fun getContatoOrdenadoPeloNome(): Flow<List<Contato>>
+
+    @Query("SELECT * FROM contatos ORDER BY sobrenome ASC")
+    fun getContatoOrdenadoPeloSobrenome(): Flow<List<Contato>>
+
+    @Query("SELECT * FROM contatos ORDER BY telefone ASC")
+    fun getContatoOrdenadoPeloTelefone(): Flow<List<Contato>>
+}
